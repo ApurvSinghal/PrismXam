2563328bf0f30bf7d2ab920f68cbc3c9823addae
ApurvSinghal <apurv.singhal28@gmail.com>
2021-04-13 14:06:27 +0530

    Add project files.

---
+++ PrismXam/PrismXam/App.xaml.cs
@@ -0,0 +1,33 @@
+using Prism;
+using Prism.Ioc;
+using PrismXam.ViewModels;
+using PrismXam.Views;
+using Xamarin.Essentials.Implementation;
+using Xamarin.Essentials.Interfaces;
+using Xamarin.Forms;
+
+namespace PrismXam
+{
+    public partial class App
+    {
+        public App(IPlatformInitializer initializer)
+            : base(initializer)
+        {
+        }
+
+        protected override async void OnInitialized()
+        {
+            InitializeComponent();
+
+            await NavigationService.NavigateAsync("NavigationPage/MainPage");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterSingleton<IAppInfo, AppInfoImplementation>();
+
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
+        }
+    }
+}
---
+++ PrismXam/PrismXam/Views/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿
+namespace PrismXam.Views
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
